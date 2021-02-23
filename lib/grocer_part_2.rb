a5d7bd821ebedfc0cfa254c92bf48384951a6f1f
Andre Hochi <andrehochiwork@gmail.com>
2021-02-23 01:11:26 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
@@ -16,6 +18,7 @@ def apply_coupons(cart, coupons)
           modified_coupon[:count] = coupon_properties[:num]
           push_later.push(modified_coupon)
           item_properties[:count] = item_properties[:count] - coupon_properties[:num]
+          binding.pry
         end
       end
     end
